dc96b461ff337357471f2cecf1f4ca48c79174ac
tri-jet <sahatrijit123@gmail.com>
2024-02-28 20:32:04 +0000

    Add basic MVP movement

--- MVPGame/Assets/script/player movement.cs
+++ MVPGame/Assets/script/player movement.cs
@@ -4,13 +4,57 @@ using UnityEngine;
 using UnityEngine.UI;
 
 // player moving from side to side
-public class movement : MonoBehaviour
-{
+public class movement : MonoBehaviour{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+    private int coinCount = 0;
+    private bool onGround;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        UpdateScoreText();
+    }
+    
+    void Update(){
+        //Left and Right Movement
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput*moveSpeed, rb.velocity.y);
+
+        //Jumping
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) && onGround)
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    void FixedUpdate(){
+         // Check if the player is grounded
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
+        onGround = (hit.collider != null);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "coin")
+        {
+            coinCount++;
+            UpdateScoreText();
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void UpdateScoreText(){
+            GameObject.Find("Number (1)").GetComponent<Text>().text = coinCount.ToString();
+    }
+
+
+
+    /*
     private Rigidbody2D rb;
     private BoxCollider2D coll;
 
     [Header("move parameter")]
-    public float speed = 8f;
+    public float peed = 8f;
     public float crouchSpeedDivisor = 3f;
 
     [Header("Jump parameter ")]
@@ -36,6 +80,8 @@ public class movement : MonoBehaviour
     bool jumpHeld;
     bool crouchHeld;
 
+    private int coinCount = 0;
+
     //Collider size
     Vector2 colliderStandSize;
     Vector2 colliderStandOffset;
@@ -50,14 +96,14 @@ public class movement : MonoBehaviour
 
         colliderStandSize = coll.size;
         colliderStandOffset = coll.offset;
-        colliderCrouchSize = new Vector2(coll.size.x, coll.size.y / 2f);
-        colliderCouchOffset = new Vector2(coll.offset.x, coll.offset.y / 2f);
+        //colliderCrouchSize = new Vector2(coll.size.x, coll.size.y / 2f);
+        //colliderCouchOffset = new Vector2(coll.offset.x, coll.offset.y / 2f);
     }
     void Update()
     {
-        jumpPressed = Input.GetButtonDown("Jump");
-        jumpHeld = Input.GetButton("Jump");
-        crouchHeld = Input.GetButton("Crouch");
+        jumpPressed = Input.GetButtonDown("Vertical");
+        //jumpHeld = Input.GetButton("Jump");
+        //crouchHeld = Input.GetButton("Crouch");
         physicsCheck();
         GroundMovement();
         MidAirMovement();
@@ -86,7 +132,7 @@ public class movement : MonoBehaviour
     void GroundMovement()
     {
 
-        if (crouchHeld && !isCrouch && isOnGround)
+        /*if (crouchHeld && !isCrouch && isOnGround)
             Crouch();
         else if (!crouchHeld && isCrouch)
             StandUp();
@@ -95,7 +141,7 @@ public class movement : MonoBehaviour
 
         xVelocity = Input.GetAxis("Horizontal");
 
-        if (isCrouch)
+        /*if (isCrouch)
             xVelocity /= crouchSpeedDivisor;
 
         rb.velocity = new Vector2(xVelocity * speed, rb.velocity.y);
@@ -125,7 +171,7 @@ public class movement : MonoBehaviour
 
         else if (isJump)
         {
-            if (jumpHeld)
+            /*if (jumpHeld)
                 rb.AddForce(new Vector2(0f, jumpHoldForce), ForceMode2D.Impulse);
             if (jumpTime < Time.time)
                 isJump = false;
@@ -133,7 +179,7 @@ public class movement : MonoBehaviour
 
     }
 
-    //The image direction changes when the player moves left or right
+    The image direction changes when the player moves left or right
     void FilpDirction()
     {
         if (xVelocity < 0)
@@ -143,7 +189,7 @@ public class movement : MonoBehaviour
     }
 
     // Crouch
-    void Crouch()
+    /*void Crouch()
     {
         isCrouch = true;
         coll.size = colliderCrouchSize;
@@ -161,10 +207,12 @@ public class movement : MonoBehaviour
     {
         if (collision.tag == "coin")
         {
-            int num = int.Parse(GameObject.Find("Number (1)").GetComponent<Text>().text);
-            num++;
-            GameObject.Find("Number (1)").GetComponent<Text>().text = num.ToString();
+            coinCount++;
+            //int num = int.Parse(GameObject.Find("Number (1)").GetComponent<Text>().text);
+            //num++;
+            GameObject.Find("Number (1)").GetComponent<Text>().text = coinCount.ToString();
             Destroy(collision.gameObject);
         }
     }
+    */
 }
